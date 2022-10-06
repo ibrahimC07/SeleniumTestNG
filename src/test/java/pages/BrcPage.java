de8635d19dc236d54a0251d3a0560eaabb475553
ibrahim <bcelikel1994@gmail.com>
2022-10-06 18:39:05 +0300

    06-10-2022

---
+++ src/test/java/pages/BrcPage.java
@@ -0,0 +1,24 @@
+package pages;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import utilities.Driver;
+
+public class BrcPage {
+
+    public BrcPage(){
+        PageFactory.initElements(Driver.getDriver(),this);
+    }
+
+    @FindBy(xpath = "//*[text()=' Login']")
+    public WebElement brclogin;
+    @FindBy(xpath = "//*[@name=\"email\"]")
+    public WebElement userEmail;
+    @FindBy(xpath = "//*[@name='password']")
+    public WebElement brcpassword;
+    @FindBy(xpath = "//*[@id='dropdown-basic-button']")
+    public WebElement basariliGiris;
+
+
+}
